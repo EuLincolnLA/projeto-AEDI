cb0bab2b38cc9d4608a3d3701b06a9e6276ab41e
Lincoln L. Amorim <lincolnlinharesamorim@gmail.com>
2024-10-29 13:51:32 -0300

    setting program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,64 @@
-﻿namespace projeto_AEDI;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace projeto_AEDI;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Bom dia pussy!");
+        Console.WriteLine("Banco de Horas - Sistema de RH");
+    }
+}
+class Funcionario
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Departamento { get; set; }
+
+    public Funcionario(int id, string nome, string departamento)
+    {
+        Id = id;
+        Nome = nome;
+        Departamento = departamento;
+    }
+
+    // Outros métodos, como AdicionarHoras
+}
+
+class BancoDeHoras
+{
+    public int FuncionarioId { get; set; }
+    public DateTime Data { get; set; }
+    public int Horas { get; set; }
+
+    public BancoDeHoras(int funcionarioId, DateTime data, int horas)
+    {
+        FuncionarioId = funcionarioId;
+        Data = data;
+        Horas = horas;
+    }
+
+    // Método para salvar no arquivo TXT
+    public void RegistrarHoras(Funcionario funcionario, int horas, DateTime data)
+    {
+        // Implementação de registro de horas
+    }
+}
+
+class Departamento
+{
+    public string Nome { get; set; }
+    public List<Funcionario> ListaFuncionarios { get; set; } = new List<Funcionario>();
+
+    public Departamento(string nome)
+    {
+        Nome = nome;
+    }
+
+    public void AdicionarFuncionario(Funcionario funcionario)
+    {
+        ListaFuncionarios.Add(funcionario);
     }
 }
--- obj/Debug/net8.0/projeto-AEDI.AssemblyInfo.cs
+++ obj/Debug/net8.0/projeto-AEDI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projeto-AEDI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b723338311e98a923ce11f6f804bd7ba03cf585")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84990708fddf5cf506b6a52a1ccf4881f03410ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("projeto-AEDI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projeto-AEDI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
